00eb9db82310df0d7e5774b6af75f5e5a2504f88
Devon Beets <dbeetzz@gmail.com>
2016-10-13 13:47:19 -0600

    updated primary load coordinates

--- js/map.js
+++ js/map.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 	// Makes the map
 	L.mapbox.accessToken = "pk.eyJ1IjoiZGJlZXRzIiwiYSI6ImNpdTRsdXIzNDBqcXkyb3BsaWZ5dDF1bWsifQ.fKKx1Q6B4vX4HadQnqDvGw";
 	var map = L.mapbox.map('map', 'mapbox.streets');
-		map.setView([35.30004, -106.5511369], 9);
+		map.setView([35.08, -106.61], 11);
 
 });
 
