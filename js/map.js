e3760169d08202c21369465ddcd208f3aba442c1
Devon Beets <dbeetzz@gmail.com>
2016-10-13 12:44:23 -0600

    corrected js

--- js/map.js
+++ js/map.js
@@ -17,7 +17,7 @@ function loadCSV(csvUrl) {
 $(document).ready(function(){
 	// Makes the map
 	L.mapbox.accessToken = "pk.eyJ1IjoiZGJlZXRzIiwiYSI6ImNpdTRsdXIzNDBqcXkyb3BsaWZ5dDF1bWsifQ.fKKx1Q6B4vX4HadQnqDvGw";
-	var map = L.mapbox.map("map", "mapbox.streets")
+	var map = L.mapbox.map("map", "mapbox.streets");
 		map.setView([35.30004, -106.5511369], 9);
 
 });
