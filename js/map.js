d8485b0b4accf0d79218869af93515c7b7f925c6
Devon Beets <dbeetzz@gmail.com>
2016-10-14 16:20:17 -0600

    combined key.js unto map.js, writing key

--- js/map.js
+++ js/map.js
@@ -6,12 +6,48 @@ function loadJSON(jsonUrl) {
 	});
 }
 
-// Loads a CSV file
-function loadCSV(csvUrl) {
-	return $.ajax({
-		url: csvUrl,
-		dataType: "text"
-	});
+//draw the map key with D3
+function drawKey() {
+	//key width and height
+	var width = 250;
+	var height = 500;
+
+	//get the key element from index
+	var key = d3.select("#key");
+	var svg = key.append("svg")
+		.attr("width", width)
+		.attr("height", height);
+
+	//define the squares and colors for each household
+	var marriedFamilyKey = svg.append("rect")
+		.attr("x", 0)
+		.attr("y", 0)
+		.attr("width", (width/10))
+		.attr("height", (height/20))
+		.style("fill", "rgb(24, 204, 37)");
+
+	var maleHouseholderKey = svg.append("rect")
+		.attr("x", 0)
+		.attr("y", 0)
+		.attr("width", (width/10))
+		.attr("height", (height/20))
+		.style("fill", "rgb(48, 170, 178)");
+
+	var femaleHouseholderKey = svg.append("rect")
+		.attr("x", 0)
+		.attr("y", 0)
+		.attr("width", (width/10))
+		.attr("height", (height/20))
+		.style("fill", "rgb(255,20,147)");
+
+	//define squares labels
+	var labelOffset = 5;
+	var maleText = "Solo Male Homeowner";
+	var femaleText = "Solo Female Homeowner";
+	var marriedText = svg.append("text")
+		.attr("x", (( width / 10) + labelOffset))
+		.attr("y", 15)
+		.text("Married Family Homeowners");
 }
 
 $(document).ready(function(){
@@ -20,5 +56,17 @@ $(document).ready(function(){
 	var map = L.mapbox.map('map', 'mapbox.streets');
 		map.setView([35.08, -106.61], 11);
 
+	overlays = L.layerGroup().addTo(map);
+
+	$.when(loadJSON("data/BernalilloCensusBlocks_Joined.json")).done(function(json) {
+		var censusJson = parseCensusJson(json);
+
+		//census blocks
+		var censusBlocks = L.mapbox.featureLayer().setGeoJSON(censusJson[0]).addTo(map);
+
+		//draw the key
+		drawKey();
+
+	});
 });
 
